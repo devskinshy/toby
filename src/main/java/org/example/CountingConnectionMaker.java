4fff661653242f3d4027d797ff2b1d9c4ed13d34
skinshy <yyshgk12@gmail.com>
2023-09-02 17:21:13 +0900

    1-30

---
+++ src/main/java/org/example/CountingConnectionMaker.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import javax.management.remote.rmi._RMIConnection_Stub;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CountingConnectionMaker implements ConnectionMaker{
+    int counter = 0;
+    private ConnectionMaker realConnectionMaker;
+
+    public CountingConnectionMaker(ConnectionMaker realConnectionMaker) {
+        this.realConnectionMaker = realConnectionMaker;
+    }
+
+    @Override
+    public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+        this.counter++;
+
+        return realConnectionMaker.makeNewConnection();
+    }
+
+    public int getCounter() {
+        return this.counter;
+    }
+}
