885893c0ac05adce811f70babf97206eac0c2ac6
skinshy <yyshgk12@gmail.com>
2023-09-02 17:23:45 +0900

    1-31

---
+++ src/main/java/org/example/CountingDaoFactory.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CountingDaoFactory {
+    @Bean
+    public ConnectionMaker realConnectionMaker() {
+//        return new DConnectionMaker();
+//        return new NConnectionMaker();
+//        return new LocalDBConnectionMaker();
+        return new ProductionDBConnectionMaker();
+    }
+
+    @Bean
+    public ConnectionMaker connectionMaker() {
+        return new CountingConnectionMaker(realConnectionMaker());
+    }
+
+    @Bean
+    public UserDao userDao() {
+        return new UserDao(connectionMaker());
+    }
+
+    /*
+    public AccountDao accountDao() {
+        return new AccountDao(connectionMaker());
+    }
+
+    public MessageDao messageDao() {
+        return new MessageDao(connectionMaker());
+    }
+     */
+}
