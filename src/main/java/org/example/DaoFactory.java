dd151c8f98061e49f6ced3b5fd5414751c29e995
skinshy <yyshgk12@gmail.com>
2023-09-02 19:00:34 +0900

    1-43

--- src/main/java/org/example/DaoFactory.java
+++ src/main/java/org/example/DaoFactory.java
@@ -2,23 +2,35 @@ package org.example;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DaoFactory {
-    @Bean // --------------------------------------------> <bean
-    public ConnectionMaker connectionMaker() { // ------->     id="connectionMaker"
-//        return new DConnectionMaker();
-//        return new NConnectionMaker();
-//        return new LocalDBConnectionMaker();
-        return new ProductionDBConnectionMaker(); // ---->     class="org.example.ProductionDBConnectionMaker" />
+//    @Bean // --------------------------------------------> <bean
+//    public ConnectionMaker connectionMaker() { // ------->     id="connectionMaker"
+////        return new DConnectionMaker();
+////        return new NConnectionMaker();
+////        return new LocalDBConnectionMaker();
+//        return new ProductionDBConnectionMaker(); // ---->     class="org.example.ProductionDBConnectionMaker" />
+//    }
+
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost:3306/toby?characterEncoding=UTF-8");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+
+        return dataSource;
     }
 
     @Bean
     public UserDao userDao() {
-//        return new UserDao(connectionMaker());
-        UserDao userDao = new UserDao();
-        userDao.setConnectionMaker(connectionMaker());
-        return userDao;
+        return new UserDao(connectionMaker());
     }
 
     /*
